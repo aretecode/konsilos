f5bcb6f15a6aebbee1f1ee7b0ef4a6682405873b
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2015-09-03 22:40:45 -0300

    Add crud for basic advice info

--- future-advices.js
+++ future-advices.js
@@ -8,6 +8,22 @@ if (Meteor.isClient) {
         return adviceList.find({}, { sort: { Advice: 1 }});
     }
   });
+
+  Template.adviceForm.events({
+      'submit #advices-form': function(event, templateInstance) {
+          event.preventDefault();
+
+          var adviceName = templateInstance.find('.adviceName').value;
+          var adviceDescription = templateInstance.find('.adviceDescription').value;
+          var adviceWhen = templateInstance.find('.adviceWhen').value;
+
+          adviceList.insert({
+              Advice: adviceName,
+              Description: adviceDescription,
+              When: adviceWhen
+          });
+      }
+  });
 }
 
 if (Meteor.isServer) {
