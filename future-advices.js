8f9305e91c889c6c2575b7f66d37502d2e3ab4c4
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2015-09-06 19:44:22 -0300

    Remove autopublish package

--- future-advices.js
+++ future-advices.js
@@ -1,33 +1,35 @@
 var adviceList = new Mongo.Collection('adviceLists');
 
 if (Meteor.isClient) {
-  Session.setDefault('counter', 0);
+    Meteor.subscribe('AdviceList');
 
-  Template.adviceListing.helpers({
-    list: function () {
-        return adviceList.find({}, { sort: { Advice: 1 }});
-    }
-  });
+    Template.adviceListing.helpers({
+        list: function () {
+            return adviceList.find({}, { sort: { Advice: 1 }});
+        }
+    });
 
-  Template.adviceForm.events({
-      'submit #advices-form': function(event, templateInstance) {
-          event.preventDefault();
+    Template.adviceForm.events({
+        'submit #advices-form': function(event, templateInstance) {
+            event.preventDefault();
 
-          var adviceName = templateInstance.find('.adviceName').value;
-          var adviceDescription = templateInstance.find('.adviceDescription').value;
-          var adviceWhen = templateInstance.find('.adviceWhen').value;
+            var adviceName = templateInstance.find('.adviceName').value;
+            var adviceDescription = templateInstance.find('.adviceDescription').value;
+            var adviceWhen = templateInstance.find('.adviceWhen').value;
 
-          adviceList.insert({
-              Advice: adviceName,
-              Description: adviceDescription,
-              When: adviceWhen
-          });
-      }
-  });
+            adviceList.insert({
+                Advice: adviceName,
+                Description: adviceDescription,
+                When: adviceWhen
+            });
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        Meteor.publish('AdviceList', function() {
+            return adviceList.find();
+        });
+    });
 }
