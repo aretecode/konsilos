22f34ff87603d9ba517924a4c69a688ee3fb9477
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2015-09-06 20:51:43 -0300

    Add singin/signup capabilities

--- server/server.js
+++ server/server.js
@@ -3,3 +3,16 @@ Meteor.startup(function () {
         return adviceList.find();
     });
 });
+
+Accounts.validateNewUser(function (user) {
+    //TODO validate email
+    return true;
+});
+
+function isAdmin(user) {
+    var adminEmails = ['lfilho@gmail.com'];
+    var adminUser = Meteor.users.findOne({ 'emails.address': adminEmails[0] });
+
+    return adminUser && adminUser._id === user._id;
+}
+
