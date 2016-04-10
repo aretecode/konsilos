ff4d3457de7fd417b67e6aa8cd24a70e4d5e719f
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2016-04-10 23:09:09 +0200

    ES2015 modules pt 1: Server and Client startup

---
+++ imports/startup/client/index.js
@@ -0,0 +1,9 @@
+import './templating.js'
+
+import './templates/footer.html'
+import './templates/home.html'
+import './templates/layout-app.html'
+import './templates/loading.html'
+import './templates/page-not-found.html'
+
+import './css/konsilos.css'
