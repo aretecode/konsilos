732de584b4bdd8132c579b3793115627797caf26
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2016-09-23 23:53:39 +0200

    Make footer future proof

--- imports/startup/client/template-helpers.js
+++ imports/startup/client/template-helpers.js
@@ -2,15 +2,17 @@
 // Global template helpers:
 ///////////////////////////
 
-Template.registerHelper('Schemas', Schemas);
-Template.registerHelper('Collections', Collections);
+Template.registerHelper('Schemas', Schemas)
+Template.registerHelper('Collections', Collections)
 
 Template.registerHelper('firstName', () => {
-    return Meteor.user() && Meteor.user().profile.name.split(' ')[0];
-});
+    return Meteor.user() && Meteor.user().profile.name.split(' ')[0]
+})
 
 Template.registerHelper('isVerifiedEmail', () => {
-    return Meteor.user() && Meteor.user().emails[0].verified;
-});
+    return Meteor.user() && Meteor.user().emails[0].verified
+})
 
-Template.registerHelper('profile', () => Meteor.user() && Meteor.user().profile);
+Template.registerHelper('profile', () => Meteor.user() && Meteor.user().profile)
+
+Template.registerHelper('currentYear', () => new Date().getFullYear())
