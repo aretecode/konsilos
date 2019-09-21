bc81511faa52c813884f96ccc8d79695f33488ed
James <aretecode@gmail.com>
2019-09-20 18:22:50 -0700

    create constants file for default data & configs

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+export const DEFAULT_ADVICE_LIST = [
+  {
+    uid: 1,
+    title: 'About Life',
+    text: 'Turning 18 years',
+    description: 'I have something very important to tell you...',
+    time: '02/05/2001',
+  },
+  {
+    uid: 2,
+    title: 'About Life',
+    text: 'Turning 18 years',
+    description: 'I have something very important to tell you...',
+    time: '02/05/2001',
+  },
+  {
+    uid: 3,
+    title: 'About Life',
+    text: 'Turning 18 years',
+    description: 'I have something very important to tell you...',
+    time: '02/05/2001',
+  },
+]
