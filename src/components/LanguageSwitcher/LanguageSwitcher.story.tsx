87edbf6e6bea0c32c6e2b9222a6582d68ae63606
James <aretecode@gmail.com>
2019-09-20 18:30:39 -0700

    components: LanguageSwitcher (init)
    tests as todos
    components as minimal flag, needs love
    story as minimal implementation
    export

---
+++ src/components/LanguageSwitcher/LanguageSwitcher.story.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { LanguageSwitcher } from './LanguageSwitcher'
+
+storiesOf('LanguageSwitcher', module).add('default', () => <LanguageSwitcher />)
