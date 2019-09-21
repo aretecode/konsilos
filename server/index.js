c80f253d3eb0cbbec7b872315b45423ce7a7d973
Michael Frohberg <mfrohberg@understood.org>
2019-09-21 05:43:59 -0400

    remove some debug

--- server/index.js
+++ server/index.js
@@ -12,55 +12,34 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const envVars = {...process.env}
+// eslint-disable-line max-statements
+const strategy = new Auth0Strategy({
+  domain: process.env.DOMAIN,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+  return done(null, profile)
+})
 
-const isNotAnNPMVar = (str) => {
-  return !str.includes('npm_')
-}
+passport.use(strategy)
 
-const cleanedEnvVars = Object
-  .keys(envVars)
-  .filter(isNotAnNPMVar)
-  .map((key) => {
-    return {
-      [key]: envVars[key]
-    }
-  })
-
-  console.log(cleanedEnvVars)
-
-
-// console.log(strategy)
+passport.serializeUser((user, done) => {
+  return done(null, user)
+})
+  
+passport.deserializeUser((user, done) => {
+  return done(null, user)
+})
 
 app.prepare().then(() => {
-
-  const strategy = new Auth0Strategy({
-    domain: process.env.DOMAIN,
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-  }, (accessToken, refreshToken, extraParams, profile, done) => {
-    return done(null, profile)
-  })
-
-  passport.use(strategy)
-
-  passport.serializeUser((user, done) => {
-    done(null, user)
-  })
-  
-  passport.deserializeUser((user, done) => {
-    done(null, user)
-  })
-  
-
   const server = express()
 
   const sess = {
     secret: 'CodingIsAwesome',
     cookie: {},
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
   }
 
   if (server.get('env') === 'production') {
@@ -73,9 +52,7 @@ app.prepare().then(() => {
 
   server.use(passport.session())
   
-
   server.all('*', (req, res) => {
-    console.log(`user has visted ${req.session.views}`)
     return handle(req, res)
   })
 
