46c981f29689cc1fa6e81e3ff83f55120e584d47
Michael Frohberg <mfrohberg@understood.org>
2019-09-21 15:27:47 -0400

    add auth handlers

--- server/index.js
+++ server/index.js
@@ -12,12 +12,16 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-// eslint-disable-line max-statements
+const userInViews = require('./userInView')
+const authRouter = require('./auth')
+const userRouter = require('./userInView')
+
+//eslint-disable-line max-statements
 const strategy = new Auth0Strategy({
   domain: process.env.DOMAIN,
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
 }, (accessToken, refreshToken, extraParams, profile, done) => {
   return done(null, profile)
 })
@@ -46,20 +50,21 @@ app.prepare().then(() => {
     sess.cookie.secure = true
   }
 
-  server.use(session(sess))
-
-  server.use(passport.initialize())
-
-  server.use(passport.session())
-  
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(port, error => {
-    if (error) {
-      throw error
-    }
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+  server
+    .use(session(sess))
+    .use(passport.initialize())
+    .use(passport.session())
+    .use(userInViews())
+    .use('/auth', authRouter)
+    .get('/user', userRouter)
+    .all('*', (req, res) => {
+      return handle(req, res)
+    })
+    
+    .listen(port, error => {
+      if (error) {
+        throw error
+      }
+      console.log(`> Ready on http://localhost:${port}`)
+    })
 })
