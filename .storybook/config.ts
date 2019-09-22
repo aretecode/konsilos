b4c79262d28238697db97b1d97bfbd8ecaa75d20
James <aretecode@gmail.com>
2019-09-22 14:17:23 -0700

    deps: add "babel-plugin-require-context-hook”
    used for testing
    
    https://github.com/storybookjs/storybook/issues/3406

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -14,7 +14,6 @@ const StyledStory = styled.div`
 const toStyledStory = story => {
   class StoryApp extends React.PureComponent {
     render() {
-      console.debug('rendering: ' + story.name)
       return React.createElement(
         StyledStory,
         null,
@@ -28,4 +27,20 @@ const toStyledStory = story => {
 
 addDecorator(toStyledStory)
 
-configure(require.context('../src', true, /\.story\.tsx$/), module)
+/**
+ * @todo https://github.com/storybookjs/storybook/issues/4479
+ */
+if (process.env.NODE_ENV === 'test') {
+  // not polyfilled in config file
+  configure(
+    (global as any).__requireContext(
+      __dirname,
+      '../src',
+      true,
+      /\.story\.tsx$/
+    ),
+    module
+  )
+} else {
+  configure(require.context('../src', true, /\.story\.tsx$/), module)
+}
