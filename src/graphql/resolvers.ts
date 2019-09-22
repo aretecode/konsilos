d98d73e52e27d63961fb2d3485b9b8f29f7bfe9e
Michael Frohberg <mfrohberg@understood.org>
2019-09-21 22:52:13 -0400

    fix for top level advices families

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -10,10 +10,10 @@ export default {
       return Promise.resolve({ ...DEFAULT_USER })
     },
     adviceList() {
-      return Promise.resolve({ ...DEFAULT_ADVICE_LIST })
+      return Promise.resolve(DEFAULT_ADVICE_LIST)
     },
     familyList() {
-      return Promise.resolve({ ...DEFAULT_FAMILY_MEMBER_LIST })
+      return Promise.resolve(DEFAULT_FAMILY_MEMBER_LIST)
     },
   },
 }
