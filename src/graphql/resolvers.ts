f917166d20fcef698f72cceac5332f10818c2d7a
James <aretecode@gmail.com>
2019-09-22 20:00:26 -0700

    graphql: create helper to simplify resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,4 +1,5 @@
 import { DEFAULT_USER } from '../constants'
+import { Response as AirtableResponse } from 'airtable'
 
 /**
  * @todo typings for airtable are wrong
@@ -13,8 +14,29 @@ const base = Airtable.base(process.env.AIRTABLE_APP_KEY)
 const Advices = base('Advices').select({ view: 'Grid view' })
 const FamilyMembers = base('FamilyMembers').select({ view: 'Grid view' })
 
-const getAdvices = Advices.firstPage
-const getFamilies = FamilyMembers.firstPage
+/**
+ * @todo import typings to replace `unknown`
+ * @todo move this into library code
+ */
+export type FirstPageCallbackType = (error: any, records: any) => unknown
+export type BaseSelectResultType = {
+  firstPage: (cb: FirstPageCallbackType) => Promise<any>
+  [key: string]: unknown
+}
+const getFirstPage = <RecordsType = unknown>(
+  baseSelect: BaseSelectResultType
+): Promise<[Error | undefined, AirtableResponse<unknown>]> =>
+  new Promise((resolve, reject) => {
+    baseSelect.firstPage(
+      (error: any, records: AirtableResponse<RecordsType>) => {
+        if (error) console.error(error)
+        resolve([error, records])
+      }
+    )
+  })
+
+const getFamilyMembers = () => getFirstPage(FamilyMembers)
+const getAdvices = () => getFirstPage(Advices)
 
 export default {
   Query: {
@@ -22,28 +44,19 @@ export default {
       return Promise.resolve({ ...DEFAULT_USER })
     },
     async adviceList() {
-      await getAdvices((error: any, records: any) => {
-        if (error) {
-          console.error(error)
-          return
-        }
-
-        return records.forEach((record: any) => {
-          console.log('Advice', { ...record.fields })
-          return { ...record.fields }
-        })
+      const [error, records] = await getAdvices()
+      if (error) return
+      return records.forEach((record: any) => {
+        console.log('Advice', { ...record.fields })
+        return { ...record.fields }
       })
     },
     async familyList() {
-      await getFamilies((error: any, records: any) => {
-        if (error) {
-          console.error(error)
-          return
-        }
-        return records.forEach((record: any) => {
-          console.log('Family Member', { ...record.fields })
-          return { ...record.fields }
-        })
+      const [error, records] = await getFamilyMembers()
+      if (error) return
+      return records.forEach((record: any) => {
+        console.log('Family Member', { ...record.fields })
+        return { ...record.fields }
       })
     },
   },
